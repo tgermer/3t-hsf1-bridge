bc5c2067637fbf37793bf8851dc4b2bc8aafc855
Tristan Germer <tgermer@me.com>
2026-06-26 17:17:18 +0200

    Add firmware project structure

--- include/Constants.h
+++ include/Constants.h
@@ -1,6 +1,7 @@
 #pragma once
 
 constexpr uint32_t BUTTON_PRESS_MS = 300;
+constexpr uint32_t LONG_BUTTON_PRESS_MS = 2000;
 
 constexpr uint32_t OPEN_TIME_MS = 26300;
 constexpr uint32_t CLOSE_TIME_MS = 27000;
