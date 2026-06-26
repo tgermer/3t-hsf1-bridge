f9dbcea5c3008f6a7775cffeddc55e0326c21bce
Tristan Germer <tgermer@me.com>
2026-06-26 21:18:25 +0200

    Refactor project configuration

--- include/Config.h
+++ include/Config.h
@@ -1,9 +1,43 @@
 #pragma once
 
-constexpr auto WIFI_SSID = "";
-constexpr auto WIFI_PASSWORD = "";
+#include <Arduino.h>
 
-constexpr auto MQTT_HOST = "";
-constexpr auto MQTT_PORT = 1883;
-constexpr auto MQTT_USERNAME = "";
-constexpr auto MQTT_PASSWORD = "";
+namespace Config
+{
+    namespace WiFi
+    {
+        constexpr auto SSID = "GeschlosseneGesellschaft";
+        constexpr auto Password = "Petrarcastrasse32";
+    }
+
+    namespace MQTT
+    {
+        constexpr auto Host = "10.20.10.11";
+        constexpr uint16_t Port = 1883;
+        constexpr auto Username = "esp32";
+        constexpr auto Password = "esp32Markise";
+    }
+
+    namespace Device
+    {
+        constexpr auto Name = "3T HSF1 Bridge";
+        constexpr auto Version = "0.1.0";
+    }
+
+    namespace Remote
+    {
+        constexpr uint32_t ButtonPressMs = 300;
+        constexpr uint32_t LongButtonPressMs = 2000;
+    }
+
+    namespace Awning
+    {
+        constexpr uint32_t OpenTimeMs = 26300;
+        constexpr uint32_t CloseTimeMs = 27000;
+    }
+
+    namespace WiFiManager
+    {
+        constexpr uint32_t ReconnectIntervalMs = 10000;
+    }
+}
--- include/Constants.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-constexpr uint32_t BUTTON_PRESS_MS = 300;
-constexpr uint32_t LONG_BUTTON_PRESS_MS = 2000;
-
-constexpr uint32_t OPEN_TIME_MS = 26300;
-constexpr uint32_t CLOSE_TIME_MS = 27000;
